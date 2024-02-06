6de4cb19e4609308bd5f56e321d56b79452f98d1
harshani <ruwanthikapemarathna@gmail.com>
2024-02-06 20:16:40 +0530

    manage CRUD operations without verifying user

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const customerController = require('../controller/CustomerController');
-const verifyUser = require('../middleware/AuthMiddleware');
+//const verifyUser = require('../middleware/AuthMiddleware');
 
-router.post('/create',verifyUser, customerController.create);
-router.get('/find-by-id',verifyUser, customerController.findById);
-router.delete('/delete-by-id',verifyUser, customerController.deleteById);
-router.put('/update',verifyUser, customerController.update);
-router.get('/find-all',verifyUser, customerController.findAll);
+// router.post('/create',verifyUser, customerController.create);
+// router.get('/find-by-id',verifyUser, customerController.findById);
+// router.delete('/delete-by-id',verifyUser, customerController.deleteById);
+// router.put('/update',verifyUser, customerController.update);
+// router.get('/find-all',verifyUser, customerController.findAll);
+
+router.post('/create', customerController.create);
+router.get('/find-by-id', customerController.findById);
+router.delete('/delete-by-id', customerController.deleteById);
+router.put('/update', customerController.update);
+router.get('/find-all', customerController.findAll);
 
 module.exports=router;
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const productController = require('../controller/ProductController');
-const verifyUser = require('../middleware/AuthMiddleware');
+// verifyUser = require('../middleware/AuthMiddleware');
 
-router.post('/create',verifyUser, productController.create);
-router.get('/find-by-id',verifyUser, productController.findById);
-router.delete('/delete-by-id',verifyUser, productController.deleteById);
-router.put('/update',verifyUser, productController.update);
-router.get('/find-all',verifyUser, productController.findAll);
+// router.post('/create',verifyUser, productController.create);
+// router.get('/find-by-id',verifyUser, productController.findById);
+// router.delete('/delete-by-id',verifyUser, productController.deleteById);
+// router.put('/update',verifyUser, productController.update);
+// router.get('/find-all',verifyUser, productController.findAll);
+
+router.post('/create', productController.create);
+router.get('/find-by-id', productController.findById);
+router.delete('/delete-by-id', productController.deleteById);
+router.put('/update', productController.update);
+router.get('/find-all', productController.findAll);
 
 
 module.exports=router;
